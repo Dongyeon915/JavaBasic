77f8d381d64d7643cb2c9f7956190d6728d31713
dongyeonkim <ehd9567@naver.com>
2022-11-29 11:54:26 +0900

    for 문

---
+++ chap04/src/For/ForPrintFromTo10Example.java
@@ -0,0 +1,10 @@
+package For;
+
+public class ForPrintFromTo10Example {
+
+  public static void main(String[] args) {
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(i);
+    }
+  }
+}
---
+++ chap04/src/Switch/SwitchStringExample.java
@@ -0,0 +1,19 @@
+package Switch;
+
+public class SwitchStringExample {
+
+  public static void main(String[] args) {
+    String position = "과장";
+
+    switch (position){
+      case "부장":
+        System.out.println("100 만원");
+        break;
+      case "과장":
+        System.out.println("50 만원");
+        break;
+      default:
+        System.out.println("300만원");
+    }
+  }
+}
