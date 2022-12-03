fd753b2f614eef74977d226e847bf4b90ac28d33
dongyeonkim <ehd9567@naver.com>
2022-12-04 00:10:40 +0900

    내용추가

--- chap04/src/test/Main.java
+++ chap04/src/test/Main.java
@@ -1,24 +1,34 @@
 package test;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
   /*
-문제
-두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+세준이는 기말고사를 망쳤다. 세준이는 점수를 조작해서 집에 가져가기로 했다. 일단 세준이는 자기 점수 중에 최댓값을 골랐다.
+이 값을 M이라고 한다. 그리고 나서 모든 점수를 점수/M*100으로 고쳤다.
+
+예를 들어, 세준이의 최고점이 70이고, 수학점수가 50이었으면 수학점수는 50/70*100이 되어 71.43점이 된다.
+
+세준이의 성적을 위의 방법대로 새로 계산했을 때, 새로운 평균을 구하는 프로그램을 작성하시오.
 
 입력
-첫째 줄에 테스트 케이스의 개수 T가 주어진다.
-
-각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+첫째 줄에 시험 본 과목의 개수 N이 주어진다. 이 값은 1000보다 작거나 같다. 둘째 줄에 세준이의 현재 성적이 주어진다.
+이 값은 100보다 작거나 같은 음이 아닌 정수이고, 적어도 하나의 값은 0보다 크다.
 
 출력
-각 테스트 케이스마다 A+B를 출력한다.
+첫째 줄에 새로운 평균을 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-2 이하이면 정답이다.
    */
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
+    int m = scanner.nextInt();
+    int[] arr = new int[m];
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = scanner.nextInt();
+    }
+      Arrays.sort(arr);
+    System.out.println((double)(arr[0]/arr[arr.length-1])*100);
     }
   }
-}
+
