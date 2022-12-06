1c081d5e3a0bba962a7bc36bcd00f01816b0bb6e
dongyeonkim <ehd9567@naver.com>
2022-12-06 17:09:46 +0900

    내용추가

--- chap04/src/cc/Utube.java
+++ chap04/src/cc/Utube.java
@@ -3,16 +3,17 @@ package cc;
 import java.util.Date;
 
 public class Utube {
+
   private String title = "한국 8강 실패";
   private String id;
   private String field;
   private String[] member;
   public static int Ucount = 0;
-  private Date date = new Date();
-  private int good;
-  private int bad;
+  public Date date = new Date();
+  private int good = 0;
+  private int bad = 0;
 
-  public Utube(String id,String field){
+  public Utube(String id, String field) {
     System.out.println(date);
     System.out.println(title + "게시글에 " + id + "님이 글을 남겼습니다");
     this.id = id;
@@ -20,28 +21,52 @@ public class Utube {
     this.Ucount++;
   }
 
-  public String getTitle(){
+  public String getTitle() {
     return this.title;
   }
 
-  public String getid(){
+  public String getid() {
     return this.id;
   }
 
-  public String getfield(){
+  public String getfield() {
     return this.field;
   }
 
-  public void getUcount(){
+  public void getUcount() {
     System.out.println(this.Ucount);
   }
 
-  public String setfield(String d){
+  public String setfield(String d) {
     this.field = d;
     System.out.println("글이 수정되었습니다");
     System.out.println(date);
     return this.field;
   }
 
+  public int setgood(int a) {
+    if (a > 1) {
+      System.out.println("1개 이상 좋아요를 누를수 없습니다");
+    } else if (a == 1) {
+      good++;
+    }
+    return this.good;
+  }
 
+  public int setbad(int c) {
+    if (c > 1) {
+      System.out.println("1개 이상 좋아요를 누를수 없습니다");
+    } else if (c == 1) {
+      bad++;
+    }
+    return this.bad;
+  }
+
+  public void getgood() {
+    System.out.println("좋아요 갯수는 " + this.good);
+  }
+
+  public void getbad() {
+    System.out.println("싫어요 갯수는 " + this.bad);
+  }
 }
--- chap04/src/cc/UtubeTest.java
+++ chap04/src/cc/UtubeTest.java
@@ -3,9 +3,9 @@ package cc;
 public class UtubeTest {
 
   public static void main(String[] args) {
-    Utube user1 = new Utube("DONG","고생했네");
-    Utube user2 = new Utube("min","한국오지마");
-    Utube user3 = new Utube("su","너도 꺼져");
+    Utube user1 = new Utube("DONG", "고생했네");
+    Utube user2 = new Utube("min", "한국오지마");
+    Utube user3 = new Utube("su", "너도 꺼져");
 
     System.out.println(user1.getfield());
     System.out.println(user2.getfield());
@@ -18,13 +18,17 @@ public class UtubeTest {
     System.out.println(user1.getTitle());
 
     user1.setfield("너 나와");
-    Utube[] mem = {user1,user2,user3};
-    System.out.println(mem.length);
+    Utube[] mem = {user1, user2, user3};
+    user1.setgood(1);
 
-    System.out.println(Utube.Ucount);
+    for (int i = 0; i < mem.length; i++) {
+      System.out.println(mem[i].getid() + ":" + mem[i].getfield());
+    }
 
+    user1.getgood();
 
-
+    user1.setbad(1);
+    user1.getbad();
 
   }
 }
